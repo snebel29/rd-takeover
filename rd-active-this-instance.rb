6e25842f6f68f16eed4379eee84200a4f2b4e17a
Rundeck Backup script <prodeng@ask.com>
2015-11-17 06:30:43 -0800

    additional functionality

--- rd-active-this-instance.rb
+++ rd-active-this-instance.rb
@@ -21,32 +21,34 @@ def run(command, working_directory="/")
   end
 end
 
-def update_rundeck_master
-  # Assumming the current instance is the master
-  command = "/usr/bin/sudo /usr/bin/chef-client"
-  exit_code, output = run(command)
-  raise exit_code if exit_code != 0
+def higher_subdomain(domain)
+  domain.split(',')[0]
 end
 
-def update_cname(cname, domain)
+def update_rundeck_master
+  # Assumming the current instance is the master
+  #command = "/usr/bin/sudo /usr/bin/chef-client"
+  #exit_code, output = run(command)
+end
+
+def update_cname(cname, master)
 
 	puts "Updating #{cname} to point to #{domain}"
-	exit_code, output = run("forge quarry rrs find --where_name #{cname}")
+	exit_code, output = run("forge quarry rrs find --where_name #{higher_subdomain(cname)}")
 
-	
-
-declare -i RESULT=0
-
-forge quarry rrs update --key $rr_id --value $master
-RESULT+=$?
-forge quarry sync zones
-RESULT+=$?
-forge quarry sync zones
-RESULT+=$?
+  if output =~ /^*.rr_id: (\d+)/
+    rr_id = $1
+    exit_code, output = run("forge quarry rrs update --key #{rr_id} --value #{higher_subdomain(master)}")
+    exit_code, output = run("forge quarry sync zones")
+  else
+    puts "Failed trying to capture rr_id from woodstove"
+  end
   
 end
 
-def update_rundeck_standby
+def update_rundeck_standby(target)
+  exit_code, output = run("run -j common/rundeck-chef-client -p ADMIN --follow -- -hostname #{target}")
+  puts output
 end
 
 
@@ -62,15 +64,15 @@ searcher = Chef::Search::Query.new.search(:environment, "name:#{environment_name
         cluster['standby'] = master
         env.save
 
-	puts "This is the new environment configuration"
-	puts JSON.pretty_generate(env.default_attributes)
+	      puts "This is the new environment configuration"
+	      puts JSON.pretty_generate(env.default_attributes)
 	
-	update_rundeck_master
-	update_cname(cluster['cname'], cluster['master'])
-	update_rundeck_standby
+	      update_rundeck_master
+	      update_cname(cluster['cname'], cluster['master'])
+	      update_rundeck_standby(cluster['standby'])
 
       else
-	puts "This instance is already the master in chef_environment: #{environment_name} so nothing to do"
+	      puts "This instance is already the master in chef_environment [#{environment_name}] so nothing to do"
       end
       
     end
